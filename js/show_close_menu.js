c624219d2050d5f82f538f97bad4ccfe20f18e14
matiasdimuro <matilaacade107@gmail.com>
2022-01-14 13:21:51 -0300

    Termino Slide JS

--- js/show_close_menu.js
+++ js/show_close_menu.js
@@ -1,4 +1,27 @@
-let show_button = document.getElementById('show-menu');
+const show_button = document.getElementById('show-menu');
 const close_button = document.getElementById('close-menu');
+const menu = document.getElementById('menu');
 
-console.log(show_button, close_button);
+show_button.addEventListener('click', slideMenu);
+close_button.addEventListener('click', slideMenu);
+
+function slideMenu() {
+
+    if (show_button.style.display !== "none") {
+
+        menu.style.transform = "scale(1)";
+        show_button.style.display = "none";
+        close_button.style.display = "inline-block";
+        close_button.style.zIndex = 150;
+
+    }
+
+    else {
+
+        menu.style.transform = "scale(0)";
+        close_button.style.display = "none";
+        show_button.style.display = "inline-block";
+
+    }
+
+}
