55681887d8b758ab2002d0b204d695ef70c08a81
matiasdimuro <matilaacade107@gmail.com>
2022-01-15 19:08:47 -0300

    Terminando slide - Arreglos CSS

--- js/show_close_menu.js
+++ js/show_close_menu.js
@@ -2,26 +2,22 @@ const show_button = document.getElementById('show-menu');
 const close_button = document.getElementById('close-menu');
 const menu = document.getElementById('menu');
 
-show_button.addEventListener('click', slideMenu);
-close_button.addEventListener('click', slideMenu);
+show_button.addEventListener('click', showMenu);
+close_button.addEventListener('click', closeMenu);
 
-function slideMenu() {
+function showMenu() {
+        
+    menu.style.transform = "translateX(0)";
+    show_button.style.display = "none";
+    close_button.style.zIndex = 150;
+    close_button.style.display = "inline-block";
 
-    if (show_button.style.display !== "none") {
+}
 
-        menu.style.transform = "scale(1)";
-        show_button.style.display = "none";
-        close_button.style.display = "inline-block";
-        close_button.style.zIndex = 150;
+function closeMenu() {
 
-    }
-
-    else {
-
-        menu.style.transform = "scale(0)";
-        close_button.style.display = "none";
-        show_button.style.display = "inline-block";
-
-    }
+    menu.style.transform = "translateX(-100vw)";
+    close_button.style.display = "none";
+    show_button.style.display = "inline-block";
 
 }
